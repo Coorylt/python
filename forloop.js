8b51a9d1958887948bc923d2e303a699b9a94ccf
Corylt <coorylt@gmail.com>
2022-12-20 15:14:08 +0100

    ajout du cours sur les fonctions

--- forloop.js
+++ forloop.js
@@ -4,3 +4,10 @@ for (let i = 0; i < 10; i++){
     console.log(i)
 }
 
+
+// bien comprendre 
+let fruits = ['abricot','baie','cerise']
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
--- loops.py
+++ loops.py
@@ -43,11 +43,19 @@ for counter in range (0,10,2):
     print(f'{counter = }')
 
 
+
+
+
+
 fruits=['abricot', 'baie','cerise']
 
-#Bouccle de type for each == pour chaque
+#Bouccle de type for each == pour chaque (boucle la plus simple)
 for i, foo in enumerate(fruits):
    print(f'{i +1}=:{foo}')
+
+
+
+
 
 
 # reversed() renvoie un itérateur économe en mémoire 
@@ -84,3 +92,28 @@ for item in my_list:
 print(f'{accumulateur = }')
 
 
+#(utile pr interchanger des élements 2 a2 )
+
+
+
+
+fruits=['abricot', 'baie','cerise']
+
+for i in range (0,len(fruits),2):
+    print(fruits[i])
+
+
+#tableau
+
+my_array=[
+    ['a', 'c'],
+    ['b','d']
+]
+
+#Boucle for dans une boucle for (pour tableau)
+# len(my_array) me donne le nombre de lignes
+for i in range(0,len(my_array)):
+    #len (my_array[0]) me donne le nombre de cologne
+    for j in range (0,len(my_array[0])):
+        print(i,j, my_array[i][j])
+#Pour aborder à partir des colones il faut intervertir le I et le J 
